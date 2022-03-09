213e32d8dc311c03201782a04e64cd656d1d0dad
Chelsea Yuan <70819287+chelsealyuan@users.noreply.github.com>
2022-03-09 12:55:11 -0600

    fixed login issues

--- login.php
+++ login.php
@@ -1,63 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require 'connectdatabase.php';
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
+header("Content-Type: application/json"); // Since we are sending a JSON response here (not an HTML document), set the MIME Type to application/json
 
-<body>
-    <?php
-    require 'connectdatabase.php';
+//Because you are posting the data via fetch(), php has to retrieve it elsewhere.
+$json_str = file_get_contents('php://input');
+//This will store the data into an associative array
+$json_obj = json_decode($json_str, true);
 
-    header("Content-Type: application/json"); // Since we are sending a JSON response here (not an HTML document), set the MIME Type to application/json
+//Variables can be accessed as such:
+$username = $json_obj['username'];
+$password = $json_obj['password'];
+//This is equivalent to what you previously did with $_POST['username'] and $_POST['password']
 
-    //Because you are posting the data via fetch(), php has to retrieve it elsewhere.
-    $json_str = file_get_contents('php://input');
-    //This will store the data into an associative array
-    $json_obj = json_decode($json_str, true);
+$stmt = $mysqli->prepare("SELECT COUNT(*), username, password FROM users WHERE username=?");
+if (!$stmt) {
+    printf("Query Prep Failed: %s\n", $mysqli->error);
+    exit;
+}
 
-    //Variables can be accessed as such:
-    $username = $json_obj['username'];
-    $password = $json_obj['password'];
-    //This is equivalent to what you previously did with $_POST['username'] and $_POST['password']
+$stmt->bind_param('s', $username);
+$stmt->execute();
 
-    $stmt = $mysqli->prepare("SELECT COUNT(*), username, password FROM users WHERE username=?");
-    if (!$stmt) {
-        printf("Query Prep Failed: %s\n", $mysqli->error);
-        exit;
-    }
+$stmt->bind_result($cnt, $username, $pwd_hash);
+$stmt->fetch();
 
-    $stmt->bind_param('s', $username);
-    $stmt->execute();
+if ($cnt == 1 && password_verify($password, $pwd_hash)) {
 
-    $stmt->bind_result($cnt, $username, $pwd_hash);
-    $stmt->fetch();
+    ini_set("session.cookie_httponly", 1);
+    session_start();
+    $_SESSION['username'] = $username;
+    $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(32));
 
-    if ($cnt == 1 && password_verify($password, $pwd_hash)) {
+    echo json_encode(array(
+        "success" => true,
+    ));
+    exit;
+} else {
+    echo json_encode(array(
+        "success" => false,
+        "message" => "Incorrect Username or Password"
+    ));
 
-        ini_set("session.cookie_httponly", 1);
-        session_start();
-        $_SESSION['username'] = $username;
-        $_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(32));
-
-        echo json_encode(array(
-            "success" => true,
-        ));
-        exit;
-    } else {
-        echo json_encode(array(
-            "success" => false,
-            "message" => "Incorrect Username or Password"
-        ));
-
-        echo "login failed";
-        exit;
-    }
-    ?>
-</body>
-
-</html>
+    echo "login failed";
+    exit;
+}
+?>F
--- register.php
+++ register.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Register </title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-
-<body>
 
     <?php
     require 'connectdatabase.php';
@@ -97,7 +85,3 @@
         exit;
     }
     ?>
-
-</body>
-
-</html>
